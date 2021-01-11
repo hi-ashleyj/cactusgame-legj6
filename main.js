ced49ed9269c24f6872f5b80ffffe69f239217bb
hi-ashleyj <ajor023@gmail.com>
2021-01-11 15:38:30 +1300

    Add levelmap also basic assets

--- main.js
+++ main.js
@@ -1,9 +1,80 @@
 Game.create({ width: 1080, height: 1920 });
 
+let LevelMap = function(tileMap, levelMap, position) {
+    this.x = position.x;
+    this.y = position.y;
+    this.size = position.size;
+    this.tiles = tileMap;
+    this.sizeY = levelMap.length;
+    this.sizeX = levelMap[0].length;
+    this.map = (new Array(this.sizeY));
+    for (let i = 0; i < this.sizeY; i ++) {
+        this.map[i] = levelMap[i].split("");
+    }
+};
+
+LevelMap.prototype.draw = function(layer) {
+    let xPos = Game.width / 2; // Calculates remaining pixels to fill
+    let fromX = Math.floor(this.x / this.size); // Calculates which tile the player is standing in
+    xPos -= this.x % this.size; // Aligns xPos to the left side of that tile
+
+    fromX -= Math.ceil(xPos / this.size); // calculates number of remaining tiles to fill the screen, then remembers that index
+    xPos -= (Math.ceil(xPos / this.size) * this.size); // Calcuates position of that tile relative to screen space
+
+    let yPos = Game.width / 2; // For Y axis
+    let fromY = Math.floor(this.y / this.size); 
+    yPos -= this.x % this.size; 
+
+    fromY -= Math.ceil(yPos / this.size); 
+    yPos -= (Math.ceil(yPos / this.size) * this.size);
+
+    // --- Prepare for heck
+
+    let localY = 0 + yPos;
+    let tileY = 0 + fromY;
+
+    while (localY < Game.height) {
+        let localX = 0 + xPos;
+        let tileX = 0 + fromX;
+
+        while (localX < Game.width) {
+            if (this.map[tileY][tileX] !== "-") {
+                this.tiles[this.map[tileY][tileX]].draw(layer, localX, localY, this.size, this.size);
+            }
+
+            localX += this.size;
+            tileX += 1;
+        }
+
+        localY += this.size;
+        tileY += 1;
+    }
+};
+
+LevelMap.prototype.move = function(x, y, size) {
+    if (typeof x == "number") { this.x += x; };
+    if (typeof y == "number") { this.y += y; };
+    if (typeof size == "number") { this.size += size; };
+}
 
 
-Game.on("loop", () => {
-    console.log(banana.value());
+
+let backgroundLayer = new Layer("background", { level: 0 });
+let mapLayer = new Layer("map", { level: 1 });
+let detailsLayer = new Layer("details", { level: 2 });
+let hud = new Layer("hud", { level: 3 });
+
+let state = "loading";
+
+let speed = 10;
+
+
+
+Game.on("loop", ({ stamp, delta }) => {
+
+
+
+
 });
 
 Game.start();
